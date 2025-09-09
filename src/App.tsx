1d26bf0434d3402ea1bfce7a7d5fdaf560f96324
Fahchouch Mohammed <fahchouchsm@gmail.com>
2025-09-09 02:15:53 +0100

    Add sidebar and theme controller with DaisyUI
    Introduced Sidebar and ThemeController components to the app, enabling a drawer sidebar and theme switching UI. Integrated DaisyUI for enhanced UI components, updated Tailwind CSS configuration, and adjusted Vite config to support Tailwind plugin. Updated index.html and App.tsx to initialize theme and layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import Sidebar from "./components/sidebar/sidebar";
+import { ThemeController } from "./components/themeController";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="h-screen bg-white">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Sidebar />
+        <ThemeController />
+      </div>
     </>
   );
 }
